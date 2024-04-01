d22efeb8ac6268a2282b6a984b6a282884cabb02
Marcelosra6 <145044515+Marcelosra6@users.noreply.github.com>
2024-04-01 17:22:01 -0500

    Propuestos

---
+++ lab03/propuesto3.cpp
@@ -0,0 +1,4 @@
+/*Programa de simulación de vuelo: Crea una clase "Misión" y “Avión” que permita a los estudiantes
+planificar una misión de vuelo. La clase debe tener atributos como el destino, la duración de la
+misión y la tripulación necesaria. Los estudiantes deben crear métodos para simular la misión y
+determinar si el avión tiene suficiente combustible para completar la misión con éxito.*/
