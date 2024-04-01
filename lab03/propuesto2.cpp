1537f1a45559093196c0dab8e4f77f7f64695d61
Marcelosra6 <145044515+Marcelosra6@users.noreply.github.com>
2024-04-01 17:24:36 -0500

    Update propuesto2.cpp

--- lab03/propuesto2.cpp
+++ lab03/propuesto2.cpp
@@ -1,4 +1,1 @@
-/*Sistema de gestión de pedidos: Crea una clase "Inventario" que tenga una lista de productos
-disponibles y una lista de productos agotados. Los estudiantes deben crear métodos para agregar
-productos al inventario y actualizar las listas de productos disponibles y agotados. También deben
-crear métodos para realizar pedidos y actualizar la lista de facturas de clientes.*/
+
